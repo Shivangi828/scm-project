edb1ddb9332f97e2a075fb8c1cf223980e483cc8
Shivangi828 <122440800+Shivangi828@users.noreply.github.com>
2023-03-21 20:30:45 +0530

    Add files via upload

---
+++ scm task 1.2/class-/program1.c
@@ -0,0 +1,23 @@
+int main()
+{
+int a,b,c;
+scanf("%d",&a);	
+scanf("%d",&b);	
+scanf("%d",&c);	
+
+if (a<c && a<b )
+{ 
+ printf("a is smaaller");
+}
+
+else if (b<c && b<a)
+{
+
+ printf("b is smalleeer");
+}
+else
+{
+	printf("c is smaller");
+}
+}
+
---
+++ scm task 1.2/class-/program2.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("hello tritiya how are you");
+	return 0;
+}
